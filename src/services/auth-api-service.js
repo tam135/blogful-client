b68f8193c80119e3d60449108c4d786f2a0ff367
Tam Phan <tnphan0204@gmail.com>
2019-08-14 02:54:01 -0400

    data protection

---
+++ src/services/auth-api-service.js
@@ -0,0 +1,15 @@
+function count(start) {
+    start = start + 1;
+    for (let i = start + 1; i < (start + 10); i++) {
+        return (start + " then " + i);
+    }
+}
+
+
+function countUp(start) {
+    start = start + 1;
+    for (let i = start + 1; i < (start + 10); i++) {
+        console.log (start + " then " + i)
+    }
+    
+}
